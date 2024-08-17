edb7320284220e34bb73f3d997eadcc7fa7e1cac
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-08-17 16:22:47 +0700

    chore: slide animation

--- src/components/burger.tsx
+++ src/components/burger.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Menu } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { menuItems } from "../types/constants/menuItems";
 import {
   useActiveBurgerMenuStore,
@@ -11,6 +11,23 @@ import {
 const BurgerMenu = () => {
   const { activeIndex, setActiveIndex } = useActiveMenuStore();
   const { toggle, setToggle } = useActiveBurgerMenuStore();
+  const [isMounted, setIsMounted] = useState(toggle);
+  const [animationSlide, setAnimationSlide] =
+    useState<string>("animate-slide-in");
+  const [overlay, setOverlay] = useState<string>("opacity-100");
+
+  useEffect(() => {
+    if (toggle) {
+      setIsMounted(true);
+      setAnimationSlide("animate-slide-in");
+      setOverlay("opacity-100");
+    } else {
+      setAnimationSlide("animate-slide-out");
+      setOverlay("opacity-0");
+      setTimeout(() => setIsMounted(false), 300);
+    }
+  }, [toggle]);
+
   return (
     <div className="">
       {!toggle && (
@@ -18,13 +35,15 @@ const BurgerMenu = () => {
           <Menu className="h-6 w-6" />
         </button>
       )}
-      {toggle && (
+      {isMounted && (
         <>
           <div
-            className="bg-slate-900/60 fixed inset-0 z-10"
+            className={`bg-slate-500/50 fixed inset-0 z-10 transition-opacity duration-300 ease-in-out ${overlay}`}
             onClick={setToggle}
           ></div>
-          <nav className="absolute z-20 bg-white h-screen w-3/4">
+          <nav
+            className={`fixed top-0 left-0 h-screen w-2/3 bg-white shadow-md z-20  transform ${animationSlide}`}
+          >
             <ul className=" mt-16">
               {menuItems.map((item, index) => (
                 <li
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -27,14 +27,24 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "slide-in": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        "slide-out": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-in": "slide-in 0.3s ease-out forwards",
+        "slide-out": "slide-out 0.3s ease-in forwards",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
