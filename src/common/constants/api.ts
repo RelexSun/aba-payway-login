b655b0b986a57ff0b54cb22ea072b622972deeff
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-10-03 18:34:02 +0700

    deploy: fixing route

--- src/common/constants/api.ts
+++ src/common/constants/api.ts
@@ -1,9 +1,9 @@
 export const API = {
   URL_ENDPOINT: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000",
   URL_AUTH_ENDPOINT:
-    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/auth",
+    process.env.NEXT_PUBLIC_API_URL + "/auth" ?? "http://localhost:4000/auth",
   URL_SHOP_ENDPOINT:
-    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/shops",
+    process.env.NEXT_PUBLIC_API_URL + "/shops" ?? "http://localhost:4000/shops",
   URL_TABLE_ENDPOINT:
-    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000/table",
+    process.env.NEXT_PUBLIC_API_URL + "/table" ?? "http://localhost:4000/table",
 };
